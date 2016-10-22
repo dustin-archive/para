c75e693a4b83121f30a039e3468408d9bb5f202c
Dustin Dowell <dustindowell22@gmail.com>
2016-10-21 21:46:40 -0500

    Remove bower && add browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,50 @@
-'use strict'
-
 const gulp = require('gulp')
+const browsersync = require('browser-sync').create()
 
-  const sass = require('gulp-sass')
-  const postcss = require('gulp-postcss')
-    const autoprefixer = require('autoprefixer')
+const sass = require('gulp-sass')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
 
-  const jade = require('gulp-jade')
+const jade = require('gulp-jade')
 
-  const browserify = require('browserify')
-  const source = require('vinyl-source-stream')
+const browserify = require('browserify')
+const source = require('vinyl-source-stream')
+const uglify = require('gulp-uglify')
+const streamify = require('gulp-streamify')
 
 gulp.task('scss', () => {
   return gulp.src('src/main.scss')
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(sass({
+      outputStyle: 'compressed',
+      includePaths: ['node_modules', 'src/components']
+    }).on('error', sass.logError))
     .pipe(postcss([ autoprefixer ]))
     .pipe(gulp.dest('dist'))
 })
 
 gulp.task('jade', () => {
-  return gulp.src('src/main.jade')
-    .pipe(jade({ pretty: true, basedir: __dirname }))
+  return gulp.src('src/index.jade')
+    .pipe(jade({
+      pretty: true,
+      globals: ['src/components'],
+      basedir: __dirname
+    }))
     .pipe(gulp.dest('dist'))
 })
 
 gulp.task('js', function() {
-  browserify('src/main.js').bundle()
+  browserify('./src/main.js', { paths: ['./src/components'] }).bundle()
     .pipe(source('main.js'))
+    .pipe(streamify(uglify()))
     .pipe(gulp.dest('dist'))
 })
 
-gulp.task('build', ['scss', 'jade', 'js'])
+gulp.task('build', ['scss', 'jade', 'js'], function (done) {
+  browsersync.reload()
+  done()
+})
 
 gulp.task('watch', () => {
+  browsersync.init({ server: { baseDir: 'dist/' } })
   gulp.watch('src/**/*', ['build'])
 })
